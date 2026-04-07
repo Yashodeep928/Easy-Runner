3f0e571c4a3e70c89fcbb49c8553910e46d302c4
Yashodeep Deshpande <yashodeepdeshpande84639@gmail.com>
2026-04-07 08:35:59 +0530

    Working on Jump logic

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-import {accessPlatform,createPlatform} from './platform.js'
+import { accessPlatform, createPlatform } from './platform.js';
+import { startJump, initJumpSystem, updateJump, isInAir } from './jump.js';
 
+accessPlatform();
+createPlatform();
 
- accessPlatform();
-console.log(createPlatform());
+initJumpSystem(0);       // pass base ground
+startJump(false);        // pass gameOver status
 
+const result = updateJump(null);  // null means no platform below
+console.log(result);
+
+console.log(isInAir());
