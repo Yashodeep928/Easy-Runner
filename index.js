dc5e745c5c32c6566b1384d9c85fa716f315fc49
Yashodeep Deshpande <yashodeepdeshpande84639@gmail.com>
2026-04-12 10:08:15 +0530

    Working on the score card

--- index.js
+++ index.js
@@ -1,11 +1,69 @@
 import { accessPlatform, createPlatform } from './platform.js';
 import { startJump, initJumpSystem, updateJump, isInAir } from './jump.js';
-
+import {
+  initCoinAndScore,
+  checkCoinCollision,
+  collectCoin
+} from "./scoreCoin.js";
+const pikachu = document.getElementById("pikachu")
+let x = 0;
+let direction = 0;
+let gameOver = false;
 accessPlatform();
 createPlatform();
 
 initJumpSystem(0);       // pass base ground
 startJump(false);        // pass gameOver status
 
-const result = updateJump(null);  // null means no platform below
-console.log(result);
+
+function animate() {
+
+  // COIN COLLISION
+  if (checkCoinCollision(pikachu)) {
+    collectCoin();
+  }
+
+  // HORIZONTAL MOVE
+  x += direction * 5;
+  x = Math.max(0, Math.min(x, maxX));
+  pikachu.style.left = x + "px";
+
+  tooltip.style.display = x === 0 ? "block" : "none";
+
+  // PLATFORM DETECTION
+  const activeGround = getGroundLevel(x, boxWidth, isInAir());
+
+  // JUMP / FALL UPDATE
+  const result = updateJump(activeGround);
+
+  if (result.dead) {
+    pikachu.style.display = "none";
+    gameOver = true;
+    return;
+  }
+
+  pikachu.style.bottom = result.groundLevel + "px";
+
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+window.addEventListener("keydown", (e) => {
+  if (e.code === "Space") {
+    startJump(false);
+  }
+   if (e.key === "ArrowRight") {
+      direction = 1;
+      pikachu.style.transform = "scaleX(1)";
+    }
+  
+    if (e.key === "ArrowLeft") {
+      direction = -1;
+      pikachu.style.transform = "scaleX(-1)";
+    }
+  
+    if (e.key === "ArrowUp") {
+      startJump(gameOver);
+    }
+});
---
+++ scoreCoin.js
@@ -0,0 +1,42 @@
+
+let coinEl;
+let scoreEl;
+
+
+export function initCoinAndScore(coinElement, scoreElement) {
+  coinEl = coinElement;
+  scoreEl = scoreElement;
+  placeCoin();
+}
+
+
+export function placeCoin() {
+  const randomLeft = Math.floor(Math.random() * 656);
+  const randomBottom = Math.floor(Math.random() * (500 - 353 + 1)) + 353;
+
+  coinEl.style.left = randomLeft + "px";
+  coinEl.style.bottom = randomBottom + "px";
+  coinEl.style.display = "block";
+}
+
+
+export function checkCoinCollision(playerEl) {
+  if (coinEl.style.display === "none") return false;
+
+  const p = playerEl.getBoundingClientRect();
+  const c = coinEl.getBoundingClientRect();
+
+  return (
+    p.left < c.right &&
+    p.right > c.left &&
+    p.top < c.bottom &&
+    p.bottom > c.top
+  );
+}
+
+export function collectCoin() {
+  const score = Number(scoreEl.innerText);
+  scoreEl.innerText = score + 1;
+  coinEl.style.display = "none";
+  placeCoin();
+}
